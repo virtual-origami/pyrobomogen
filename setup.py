b002f893207f0161e31a290fec33089968f382d7
Shantanoo Desai <shantanoo.desai@gmail.com>
2021-03-30 20:11:21 +0200

    chore(setup): lint

--- setup.py
+++ setup.py
@@ -1,25 +1,27 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 
 
 def readme():
-    with open( 'README.md' ) as f:
+    with open('README.md') as f:
         return f.read()
 
 
-setup( name='RoboGen',
-       version='0.1',
-       description='Robot Motion Generator',
-       long_description=readme(),
-       author='Karthik , Shan Desai',
-       author_email='des@biba.uni-bremen.de, she@biba.uni-bremen.de',
-       license='MIT',
-       packages=['RoboGen'],
-       scripts=[],
-       install_requires=[
-           'numpy',
-           'PyYAML',
-           'pamqp',
-           'aio-pika'
-       ],
-       include_data_package=True,
-       zip_safe=False )
+setup(
+    name='RoboGen',
+    version='0.1',
+    description='Robot Motion Generator',
+    long_description=readme(),
+    author='Karthik , Shan Desai',
+    author_email='she@biba.uni-bremen.de, des@biba.uni-bremen.de',
+    license='MIT',
+    packages=['RoboGen'],
+    scripts=[],
+    install_requires=[
+        'numpy',
+        'PyYAML',
+        'pamqp',
+        'aio-pika'
+    ],
+    include_data_package=True,
+    zip_safe=False
+)
