0aef8ccf06685ff77669f30b148f512b1bdd762a
Shantanoo Desai <shantanoo.desai@gmail.com>
2021-03-31 18:16:10 +0200

    dev(module): refactor __init__ file for RoboGen

--- RoboGen/__init__.py
+++ RoboGen/__init__.py
@@ -1,2 +1,12 @@
-from .model import *
-from .AMQPubSub import *
+from __future__ import generator_stop
+from __future__ import annotations
+
+from .model import RobotArm2
+from .AMQPubSub import AMQ_Pub_Sub
+
+__all__ = [
+    'RobotArm2',
+    'AMQ_Pub_Sub'
+]
+
+__version__ = '0.0.1'
