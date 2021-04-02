b89f38218267876ffda801b91e41a5a9b0d7b11a
Shantanoo Desai <shantanoo.desai@gmail.com>
2021-04-02 13:42:53 +0200

    refactor(main): resolve flake8 lint errors

--- main.py
+++ main.py
@@ -10,31 +10,31 @@ import yaml
 
 from RoboGen.model import RobotArm2
 
-logging.basicConfig( stream=sys.stdout, level=logging.DEBUG )
-logger = logging.getLogger( __name__ )
+logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
+logger = logging.getLogger(__name__)
 
-handler = logging.FileHandler( '/tmp/robogen.log' )
-handler.setLevel( logging.ERROR )
+handler = logging.FileHandler('/tmp/robogen.log')
+handler.setLevel(logging.ERROR)
 
 asyncio_logger = logging.getLogger('asyncio')
 asyncio_logger.setLevel(logging.WARNING)
 
-formatter = logging.Formatter( '%(levelname)-8s-[%(filename)s:%(lineno)d]-%(message)s' )
-handler.setFormatter( formatter )
-logger.addHandler( handler )
+formatter = logging.Formatter('%(levelname)-8s-[%(filename)s:%(lineno)d]-%(message)s')
+handler.setFormatter(formatter)
+logger.addHandler(handler)
 
 is_sighup_received = False
 
 
 def parse_arguments():
     """Arguments to run the script"""
-    parser = argparse.ArgumentParser( description='Robot motion generator' )
-    parser.add_argument( '--config', '-c', required=True, help='YAML Configuration File for RobotMotionGen with path' )
+    parser = argparse.ArgumentParser(description='Robot motion generator')
+    parser.add_argument('--config', '-c', required=True, help='YAML Configuration File for RobotMotionGen with path')
     return parser.parse_args()
 
 
 def signal_handler(name):
-    print( f'signal_handler {name}' )
+    logger.debug(f'signal_handler {name}')
     global is_sighup_received
     is_sighup_received = True
 
@@ -47,12 +47,12 @@ async def app(eventloop, config):
     while True:
         # Read configuration
         try:
-            generator_config = read_config( config )
+            generator_config = read_config(config)
         except Exception as e:
             logger.error(f'Error while reading configuration: {e}')
             break
 
-        logger.debug("Robot Generator Version: %s",generator_config['version'])
+        logger.debug("Robot Generator Version: %s", generator_config['version'])
 
         # check if amq or mqtt key description present in configuration
         if ("amq" not in generator_config) and ("mqtt" not in generator_config):
@@ -101,24 +101,25 @@ async def app(eventloop, config):
 
 def read_config(yaml_config_file):
     """Parse the given Configuration File"""
-    if os.path.exists( yaml_config_file ):
-        with open( yaml_config_file, 'r' ) as config_file:
-            yaml_as_dict = yaml.load( config_file, Loader=yaml.FullLoader )
+    if os.path.exists(yaml_config_file):
+        with open(yaml_config_file, 'r') as config_file:
+            yaml_as_dict = yaml.load(config_file, Loader=yaml.FullLoader)
         return yaml_as_dict['robot_generator']
     else:
         raise FileNotFoundError
+        logger.error('YAML Configuration File not Found.')
 
 
 def main():
     """Initialization"""
     args = parse_arguments()
-    if not os.path.isfile( args.config ):
-        logging.error( "configuration file not readable. Check path to configuration file" )
-        sys.exit( -1 )
+    if not os.path.isfile(args.config):
+        logging.error("configuration file not readable. Check path to configuration file")
+        sys.exit(-1)
 
     event_loop = asyncio.get_event_loop()
-    event_loop.add_signal_handler( signal.SIGHUP, functools.partial( signal_handler, name='SIGHUP' ) )
-    event_loop.run_until_complete( app( event_loop, args.config ) )
+    event_loop.add_signal_handler(signal.SIGHUP, functools.partial(signal_handler, name='SIGHUP'))
+    event_loop.run_until_complete(app(event_loop, args.config))
 
 
 if __name__ == "__main__":
