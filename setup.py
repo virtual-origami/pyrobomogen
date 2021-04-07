a80d094cc85b410fa526255eb6b3f67b7e77cd75
Shan Desai <des@biba.uni-bremen.de>
2021-04-07 18:00:57 +0200

    chore: update setup.py
    - make requirements read from `requirements.txt`
    - read version from `pyrobomogen` module
    - refactor other setup information

--- setup.py
+++ setup.py
@@ -1,3 +1,5 @@
+import os
+import re
 from setuptools import setup, find_packages
 
 
@@ -6,22 +8,26 @@ def readme():
         return f.read()
 
 
+with open(os.path.join(os.path.dirname(__file__), 'pyrobomogen', '__init__.py')) as f:
+    version = re.search("__version__ = '([^']+)'", f.read()).group(1)
+
+
+with open('requirements.txt') as reqs_f:
+    reqs = reqs_f.read().strip().split('\n')
+
+
 setup(
     name='pyrobogen',
-    version='0.5.1',
+    version=version,
     description='Two-Joint Robotic Arm Motion Generator',
     url='https://github.com/virtual-origami/pyrobomogen',
     long_description=readme(),
-    author='Karthik Shenoy, Shan Desai',
+    long_description_content_type='text/markdown',
+    author='Karthik Shenoy Panambur, Shantanoo Desai',
     author_email='she@biba.uni-bremen.de, des@biba.uni-bremen.de',
-    license='MIT',
+    license='MIT License',
     packages=find_packages(),
-    install_requires=[
-        'numpy',
-        'PyYAML',
-        'pamqp',
-        'aio-pika'
-    ],
+    install_requires=reqs,
     include_data_package=True,
     zip_safe=False
 )
